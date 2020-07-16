25b5fc8ae11da92eaf779ca09d6ecee7286941f5
KaranDaCoder <karankhot2@gmail.com>
2020-07-16 12:24:11 -0700

    display and print SLL

--- DataStructuresAndAlgorithms/src/com/dsandalgos/linkedlists/CreateSinglyLinkedLists.java
+++ DataStructuresAndAlgorithms/src/com/dsandalgos/linkedlists/CreateSinglyLinkedLists.java
@@ -13,17 +13,47 @@ public class CreateSinglyLinkedLists {
 		}
 	}
 
+	// display linked list
+	public void displayLinkedList() {
+		Node curr_node = head;
+
+		while (curr_node != null) {
+			System.out.print(curr_node.data + " --->");
+			curr_node = curr_node.next;
+		}
+
+		System.out.println("null");
+	}
+
+	// find length of LinkedList
+	public int getLengthOfLinkedList() {
+		Node curr_node = head;
+		int count = 0;
+		while (curr_node != null) {
+			count++;
+			curr_node = curr_node.next;
+		}
+
+		System.out.println("Length of LinkedList == " + count);
+		return count;
+	}
+
 	public static void main(String[] args) {
 
 		CreateSinglyLinkedLists createSinglyLinkedLists = new CreateSinglyLinkedLists();
-		createSinglyLinkedLists.head = new Node(11);
-		Node second = new Node(22);
-		Node third = new Node(99);
-		Node fourth = new Node(8);
+		createSinglyLinkedLists.head = new Node(11); // 11|null
+		Node second = new Node(22); // 22|null
+		Node third = new Node(99); // 99|null
+		Node fourth = new Node(8); // 8|null
 
 		createSinglyLinkedLists.head.next = second;
 		second.next = third;
 		third.next = fourth;
+
+		// display the linkedlist
+		createSinglyLinkedLists.displayLinkedList();
+		// find length of LinkedList
+		createSinglyLinkedLists.getLengthOfLinkedList();
 	}
 
 }
