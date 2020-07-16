f9a1c342456436aa031c1245353275fe61d2663e
KaranDaCoder <karankhot2@gmail.com>
2020-07-16 15:56:55 -0700

    linkedListImplementation

--- DataStructuresAndAlgorithms/src/com/dsandalgos/linkedlists/CreateSinglyLinkedLists.java
+++ DataStructuresAndAlgorithms/src/com/dsandalgos/linkedlists/CreateSinglyLinkedLists.java
@@ -13,6 +13,29 @@ public class CreateSinglyLinkedLists {
 		}
 	}
 
+	// Inserting a new node at start == creating a head node
+	public void insertNodeAtStart(int data) {
+		Node new_node = new Node(data); // create a new node data | null
+		new_node.next = head; // point next of new node to head
+		head = new_node; // assign head as new node
+	}
+
+	// Inserting a new node at the end of linked list
+	public void insertNodeAtEnd(int data) {
+		Node new_node_end = new Node(data);
+
+		if (head == null) {
+			head = new_node_end;
+			return;
+		}
+
+		Node curr_node = head;
+		while (curr_node.next != null) {
+			curr_node = curr_node.next;
+		}
+		curr_node.next = new_node_end;
+	}
+
 	// display linked list
 	public void displayLinkedList() {
 		Node curr_node = head;
@@ -45,12 +68,23 @@ public class CreateSinglyLinkedLists {
 		Node second = new Node(22); // 22|null
 		Node third = new Node(99); // 99|null
 		Node fourth = new Node(8); // 8|null
-
 		createSinglyLinkedLists.head.next = second;
 		second.next = third;
 		third.next = fourth;
+		System.out.println("Linked list and it's length before inserting a node at start... ");
+		createSinglyLinkedLists.displayLinkedList();
+		createSinglyLinkedLists.getLengthOfLinkedList();
 
+		createSinglyLinkedLists.insertNodeAtStart(18);
+		System.out.println("Linked list and it's length After inserting a node at start... ");
 		// display the linkedlist
+		createSinglyLinkedLists.displayLinkedList();
+		// find length of LinkedList
+		createSinglyLinkedLists.getLengthOfLinkedList();
+		
+		createSinglyLinkedLists.insertNodeAtEnd(44);
+		createSinglyLinkedLists.insertNodeAtEnd(144);
+		createSinglyLinkedLists.insertNodeAtStart(124);
 		createSinglyLinkedLists.displayLinkedList();
 		// find length of LinkedList
 		createSinglyLinkedLists.getLengthOfLinkedList();
