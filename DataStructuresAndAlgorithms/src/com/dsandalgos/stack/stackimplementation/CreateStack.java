a9a1cbf15c154d2c9c40e2eb3ffc392e748453fd
KaranDaCoder <karankhot2@gmail.com>
2020-07-27 11:15:06 -0700

    stackLinkedListImplementation

--- DataStructuresAndAlgorithms/src/com/dsandalgos/stack/stackimplementation/CreateStack.java
+++ DataStructuresAndAlgorithms/src/com/dsandalgos/stack/stackimplementation/CreateStack.java
@@ -1,5 +1,7 @@
 package com.dsandalgos.stack.stackimplementation;
 
+import java.util.EmptyStackException;
+
 public class CreateStack {
 	private Node top;
 	private int length;
@@ -19,17 +21,52 @@ public class CreateStack {
 		length = 0;
 	}
 
+	// get stack length
 	public int getLength() {
 		return length;
 	}
 
 	public boolean isStackEmpty() {
-		return length > 0;
+		return length < 0;
+	}
+
+	// push element inside a stack
+	public void push(int data) {
+		Node node = new Node(data);
+		node.next = top;
+		top = node;
+		length++;
+	}
+
+	// pop an element from the stack
+	public int pop() {
+		if (isStackEmpty())
+			throw new EmptyStackException();
+		
+		int res = top.data;
+		top = top.next;
+		length--;
+		return res;
+	}
+
+	// get top-most element in stack
+	public int peek() {
+
+		if (isStackEmpty())
+			throw new EmptyStackException();
+		
+		return top.data;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		CreateStack createStack = new CreateStack();
+		createStack.push(10);
+		createStack.push(20);
+		createStack.push(30);
+		System.out.println(createStack.peek());
+		createStack.pop();
+		System.out.println(createStack.peek());
 	}
 
 }
